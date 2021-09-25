5b30191a238d15b50fe3566beed8a80e9b7c08be
Billo Diallo <billodiallonet@gmail.com>
2021-09-25 09:16:27 +0300

    Add valadates for username,name,age

--- challenge.rb
+++ challenge.rb
@@ -2,6 +2,10 @@ module ActiveModel
     module Validations
       module ClassMethods
 
+        validates :username, presence: true
+        validates :name, presence: true, allow_blank: false
+        validates :age, inclusion: { in: 0..9 }
+
       end
     end
 end
