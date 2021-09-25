3705ab928230624fae7ca1ba14ee9d2416be07a4
Billo Diallo <billodiallonet@gmail.com>
2021-09-25 09:17:56 +0300

    validates for username,firstname,password,token

--- challenge.rb
+++ challenge.rb
@@ -5,6 +5,11 @@ module ActiveModel
         validates :username, presence: true
         validates :name, presence: true, allow_blank: false
         validates :age, inclusion: { in: 0..9 }
+        validates :age, numericality: true
+       validates :username, presence: true
+       validates :first_name, length: { maximum: 30 }
+       validates :password, presence: true, confirmation: true, if: :password_required?
+       validates :token, length: 24, strict: TokenLengthException
 
       end
     end
